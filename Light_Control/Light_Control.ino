ed9dcba39fcc14d440214e3e7fbcc911bfc9de95
almoraleslozano <almoraleslozano@gmail.com>
2013-12-08 21:37:13 +0100

    Some changes in structure

--- Light_Control/Light_Control.ino
+++ Light_Control/Light_Control.ino
@@ -2,14 +2,12 @@
     and an external input
 */
 
-void setup_LightControl(int pinExtInput, int pinRelay1, int pinRelay2){
+void setup_LightControl(int pinExtInput, int pinRelay1, int pinRelay2, int stateRelay1, int stateRelay2){
   // pin setup
   pinMode(pinExtInput, INPUT);
   pinMode(pinRelay1, OUTPUT);
   pinMode(pinRelay2, OUTPUT);
-}
-
-void preparation_LightControl(int pinRelay1, int pinRelay2, int stateRelay1, int stateRelay2){
+  
   // is necessary to ensure that the relays have different states
   digitalWrite(pinRelay1, HIGH);
   stateRelay1 = HIGH;
@@ -23,13 +21,13 @@ void perform_LightControl(int stateExtInput, int previousInputState, int pinExtI
                             
   stateExtInput = digitalRead(pinExtInput);  
   // only serve requests with a lag to avoid rebound
-  if(stateExtInput == HIGH && previousInputState == LOW && millis() - time > debounce) {
+  if(stateExtInput == HIGH && previousInputState == LOW && (millis() - time > debounce)) {
     if(stateRelay1 == HIGH){
       stateRelay1 = LOW;
       stateRelay2 = HIGH;
     } else {
-       stateRelay1 = HIGH; 
-       stateRelay2 = LOW; 
+      stateRelay1 = HIGH; 
+      stateRelay2 = LOW; 
     }
     time = millis();
   }
--- Light_Control/Main.ino
+++ Light_Control/Main.ino
@@ -1,3 +1,6 @@
+long time = 0;
+long debounce = 500;
+
 int pinExtInput = 8;
 int pinRelay1 = 2;
 int pinRelay2 = 3;
@@ -5,23 +8,14 @@ int stateRelay1;
 int stateRelay2;
 int stateExtInput;
 int previousInputState = LOW;
-long time = 0;
-long debounce = 500;
-boolean firstTime = true;
 
 void setup() {
-  setup_LightControl(pinExtInput, pinRelay1, pinRelay2);
+  setup_LightControl(pinExtInput, pinRelay1, pinRelay2, stateRelay1, stateRelay2);
 }
 
-void loop() {
-  
-  if(firstTime){
-    preparation_LightControl(pinRelay1, pinRelay2, stateRelay1, stateRelay2);
-    firstTime = false;
-  }
+void loop() {  
   perform_LightControl(stateExtInput, previousInputState, pinExtInput, pinRelay1,
-                          pinRelay2, stateRelay1, stateRelay2, time, debounce);
-                          
+                          pinRelay2, stateRelay1, stateRelay2, time, debounce);             
 }
 
 
