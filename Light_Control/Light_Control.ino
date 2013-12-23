2704c8ee940f264bb00a64fa395f158e7cfe251e
almoraleslozano <almoraleslozano@gmail.com>
2013-12-23 23:35:36 +0100

    It works faster now

--- Light_Control/Light_Control.ino
+++ Light_Control/Light_Control.ino
@@ -22,29 +22,31 @@ void perform_LightControl(int* stateExtInput, int* previousInputState, int pinEx
                           int pinRelay1, int pinRelay2, int* stateRelay1, int* stateRelay2,
                           long* time, long debounce){
   
-  char input;
+  char* input;
   
-  // read from thee serial port is possible
+  // read from the serial port if it's possible
   if(Serial.available()){
-      input = Serial.read();
-      //delay(5);
+    *input = Serial.read();
     
-    if(input == '0'){
+    if(*input == '0'){
     // toggle relay states
       if(*stateRelay1 == HIGH){
         *stateRelay1 = LOW;
         *stateRelay2 = HIGH;
         Serial.println("cambio 1");
+        //Serial.println(*input);
       } else {
         *stateRelay1 = HIGH; 
         *stateRelay2 = LOW; 
         Serial.println("cambio 2");
+        //Serial.println(*input);
       }
       digitalWrite(pinRelay1, *stateRelay1);
       delay(10);
       digitalWrite(pinRelay2, *stateRelay2);
       delay(1000);
     } 
+    Serial.flush();
   }
   
   /*            
