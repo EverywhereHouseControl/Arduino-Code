c895ce070048c7ae34c274dd2712b0dc8c0cf6db
HectorGPala <hgutierrez2501@gmail.com>
2014-03-20 23:26:53 +0100

    modified to test new control IR

--- Test/Test.ino
+++ Test/Test.ino
@@ -19,25 +19,28 @@ void setup(){
 }
 
 void loop(){
+  loop_IR();
   if(Serial.available()){
     delay(20);  //  necesary to wait the input to be ready
     char input[16];
     int i=0;
     while(Serial.available()){
       input[i] = Serial.read();
-      i++;
-      
+      i++;      
     }    
+    
     i=i-1;  //  because one character is used to difference functionalities
     
     char function = input[i];
+    //Serial.println("hola");
+    //Serial.println(i);
     input[i] = '\0';
     switch(function){
       case '0':    
         perform_LightControl(input, i, pinRelay1, &stateRelay1); 
         break;
       case '1':    
-        loop_IR(input, i); 
+        IR(input, i); 
         break;
       default:
         Serial.println("-1");
