2bd63ca407823e8a88467a722fa4366c244e3bd0
HectorGPala <hgutierrez2501@gmail.com>
2014-04-24 23:51:24 +0200

    new version of Control_IR

--- Control_IR/Control_IR.ino
+++ Control_IR/Control_IR.ino
@@ -1,39 +1,66 @@
-/* Este modulo es un test de los modulos Control_IR y Light_Control
-Devuelve
-  0  -->  No hay error
-  1  -->  La entrada no tiene el formato "0xXXXXXXXX" donde X es un digito hexadecimal
-*/
-
+/*test sobre control IR completo*/
 #include <IRremote.h>
-#include <Base_IR.h>
 
-void setup_IR(){ }  // no es necesario inicializar nada en este módulo
+int pinRead_IR = 2;
+IRrecv recIR(pinRead_IR);
+IRsend ir;
+decode_results r;
 
-void loop_IR(char cadena[], int longitud) { 
+void setup_IR(){
+  //Serial.begin(9600);  
+  recIR.enableIRIn();
+}
 
-  if(longitud == 10){
-       IRsend irsend;
-       irsend.sendNEC(atoi(cadena), 32);
-       
-       /*
-       int i = atoi(cadena);
-       irsend.sendNEC(datos1[i],32);
-       */
-       Serial.println("0");    // Respuesta OK
-       
-       /*
-       Serial.println("Señal Enviada");
-       Serial.print("input : ");
-       Serial.print(cadena);
-       Serial.print(" --> ");
-       Serial.println(datos1[i], 16);
-       */
-       delay(500);
-   }
-   else {
-       Serial.println("1");    // Respuesta de error
+void loop_IR(){
+  if(recIR.decode(&r)) {
+    Serial.println(r.value, DEC);
+    recIR.resume();
   }
-}  
-  
-  
-  
+}
+void IR(String s){
+        int l = s.length()+1;
+        //Serial.println(s);
+        char input[l];
+        s.toCharArray(input,l);
+        int i = 0;
+	char c = input[i]; i++;
+	switch(c){
+		case '0': {
+			char datac[10];
+			datac[0] = input[i]; i++;
+			int protocol = atoi(datac);
+			datac[0] = input[i]; i++;
+			datac[1] = input[i]; i++;
+			int nbits = atoi(datac);
+                        int j = 0;
+			while(i < l){
+				datac[j] = input[i];
+				i++; j++;
+			}
+			unsigned long data = atol(datac);
+                        //Serial.println(data);
+			ir.sendIR(data,nbits,0,protocol);
+                        recIR.enableIRIn();
+			Serial.println("0");
+                        //delay(500);
+			break;
+		}
+		case '1': {
+			char buttons[2];
+			int j = 0;
+			while(i < l){
+				if(j < 2){    
+					buttons[j] = input[i]; j++; i++;
+                                }
+				else
+					j++;    
+			}
+		recIR.configIR(atoi(buttons));
+		break;
+		}
+                default:
+                Serial.println("1");
+    }
+}
+
+
