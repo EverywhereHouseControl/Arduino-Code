d6b25132ee56682f6ecac33d5532543602ccf789
HectorGPala <hgutierrez2501@gmail.com>
2014-04-25 02:46:22 +0200

    add timestamp to signal

--- Ficheros Raspberry/listenerUSB.php
+++ Ficheros Raspberry/listenerUSB.php
@@ -4,7 +4,12 @@ function listener(){
 		$fp =fopen('/dev/ttyACM0','r');//truncate
 		sleep(1);
 		while(true){
-			echo fread($fp,64);
+			$read = fread($fp,64);
+                        if($read != 0){
+                                echo "FECHA: "; echo date("d-m-Y "); 
+				echo "HORA: "; echo date("H:i:s\n\r");
+                                echo "SEÑAL: "; echo $read; echo "\n\r";
+                        }
 			sleep(2);
 		}	
 		fclose($fp);
