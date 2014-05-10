645e287cf632c75efa198b7324dc57f4a5167c3a
HectorGPala <hgutierrez2501@gmail.com>
2014-05-10 18:04:11 +0200

    now send UPDATE/INTERRUPT to server and create log in raspberry

--- Ficheros Raspberry/listenerUSB.php
+++ Ficheros Raspberry/listenerUSB.php
@@ -4,11 +4,19 @@ function listener(){
 		$fp =fopen('/dev/ttyACM0','r');//truncate
 		sleep(1);
 		while(true){
-			$read = fread($fp,64);
+			$read = "UPDATE-46-192-0N";//fread($fp,64);
                         if($read != 0){
-                                echo "FECHA: "; echo date("d-m-Y "); 
+				list($command, $idDevice, $idService, $data) = explode("-", $read);
+				header("Location: ehcontrol.net/EHControlConnect/updateState.php
+?command=$command&iddevice=$idDevice&idservice=$idService&action=0&data=$data");
+				$fp2 = fopen('/home/ehc/log.txt','a+');
+				$date = date("d-m-Y");
+				$hour = date("H:i:s");
+				fwrite($fp2,"FECHA: $date HORA: $hour SEÑAL: $read \n\r");
+				fclose($fp2);				                                
+				/*echo "FECHA: "; echo date("d-m-Y "); 
 				echo "HORA: "; echo date("H:i:s\n\r");
-                                echo "SEÑAL: "; echo $read; echo "\n\r";
+                                echo "SEÑAL: "; echo $read; echo "\n\r";*/
                         }
 			sleep(2);
 		}	
