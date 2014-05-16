0ee6bbb1c78dadd1b6d70d5c66d4f086e4fef149
HectorGPala <hgutierrez2501@gmail.com>
2014-05-16 02:42:42 +0200

    modified to standar

--- Motion_Sensor/Motion_Sensor.ino
+++ Motion_Sensor/Motion_Sensor.ino
@@ -1,27 +1,16 @@
-const int ledPin= 13;
 const int inputPin= 8;
 unsigned long last_time;//last time sensor was activated in milliseconds
 
-void setup(){
-   pinMode(ledPin, OUTPUT);
+void setup_Motion(){
    pinMode(inputPin, INPUT);
-   Serial.begin(9600);
    last_time = 0;//first time is 0 ms
    
 }
 
-void loop(){
-
-   int value= digitalRead(inputPin);
-   
-   if (((value==HIGH) && (last_time==0 )) || ((value==HIGH) && (last_time+10000<millis()))) {
-      digitalWrite(ledPin, HIGH);
-      delay(100);
-      digitalWrite(ledPin, LOW);
-      Serial.println("ON");
-      delay(100);
-      last_time=millis();
+void loop_Motion(int idDevice,idService){
+	int value= digitalRead(inputPin);
+   	if (((value==HIGH) && (last_time==0 )) || ((value==HIGH) && (last_time+10000<millis()))) {
+      		Serial.print("UPDATE-"); Serial.print(idDevice); Serial.print("-"); Serial.print(idService); Serial.print("-Movimiento");
+      		last_time=millis();
    }
-  //arduino sends "ON" to rasp when sensor is activated and has happened 10 seconds from last activation.
-
 }
